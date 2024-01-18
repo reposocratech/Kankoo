85d604b2021360f3ee5887fb8a56264b4ca2ffec
edson <edsonaristiguieta@gmail.com>
2024-01-18 13:18:57 +0100

    RegisterForm

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const usersControllers = require("../controllers/usersControllers");
 var router = express.Router();
+
 //http://localhost:3000/registeruser
 router.post("/registeruser", usersControllers.registerUser);
 
