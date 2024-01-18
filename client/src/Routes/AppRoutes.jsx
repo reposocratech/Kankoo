b92e4f109981e5e637e2089b9e803131d10da2f9
celia <celiagarcia0315@gmail.com>
2024-01-18 14:03:36 +0100

    menu user in progress

--- client/src/Routes/AppRoutes.jsx
+++ client/src/Routes/AppRoutes.jsx
@@ -4,7 +4,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBarApp from "../components/Navbar/NavbarApp";
 import { ToursGallery } from "../pages/tours/HomeToursGallery/ToursGallery";
 import { UserProfile } from "../pages/users/UserProfile/UserProfile";
-
+import { MyTours } from "../pages/users/MyTours/MyTours";
+import { Favorites } from "../pages/users/Favorites/Favorites";
+import { EditUser } from "../pages/users/EditUser/EditUser";
+import { BoughtTours } from "../pages/users/BoughtTours/BoughtTours";
+import { CreateTour } from "../pages/tours/CreateTour/CreateTour";
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -13,6 +17,11 @@ export const AppRoutes = () => {
         <Routes>
           <Route path="/" element={<ToursGallery />} />
           <Route path="/users/userprofile" element={<UserProfile />} />
+          <Route path="/users/mytours" element={<MyTours />} />
+          <Route path="/users/favtours" element={<Favorites />} />
+          <Route path="/users/boughttours" element={<BoughtTours />} />
+          <Route path="/users/edituser" element={<EditUser />} />
+          <Route path="/tours/newguide" element={<CreateTour />} />
         </Routes>
       </Row>
     </BrowserRouter>
