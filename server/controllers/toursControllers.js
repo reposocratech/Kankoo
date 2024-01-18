115dd1f98372d8974bef2cf4ffa34944204851ed
celia <celiagarcia0315@gmail.com>
2024-01-18 13:08:52 +0100

    rutas de user

--- server/controllers/toursControllers.js
+++ server/controllers/toursControllers.js
@@ -3,12 +3,10 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
-
 class toursControllers {
-
-
- 
+  newTour = (req, res) => {
+    console.log("aqui puedes crear un nuevo tour");
+  };
 }
-
 
 module.exports = new toursControllers();
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,11 @@
-var express = require('express');
-const usersControllers = require('../controllers/usersControllers');
+var express = require("express");
+const usersControllers = require("../controllers/usersControllers");
 var router = express.Router();
 
+router.get("/userprofile", usersControllers.viewProfile);
+router.get("/mytours", usersControllers.ownTours);
+router.get("/favtours", usersControllers.favTours);
+router.get("/boughttours", usersControllers.boughtTours);
+router.get("/edituser", usersControllers.editUser);
 
 module.exports = router;
