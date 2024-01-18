097d23de1804d249a66f20c2e909a841937e6c41
edson <edsonaristiguieta@gmail.com>
2024-01-18 14:29:18 +0100

    cambio intermedio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AppRoutes } from "./Routes/AppRoutes";
 import { Container } from "react-bootstrap";
-import { RegisterForm } from "./pages/auth/Register/RegisterForm";
-
 function App() {
   return (
     <Container fluid>
