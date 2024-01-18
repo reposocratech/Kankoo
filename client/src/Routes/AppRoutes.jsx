e23f06d27dea50f6622a195d1fb7c55bb0b0b009
celia <celiagarcia0315@gmail.com>
2024-01-18 13:08:52 +0100

    rutas de user

--- client/src/Routes/AppRoutes.jsx
+++ client/src/Routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Navbar, Row } from "react-bootstrap";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBarApp from "../components/Navbar/NavbarApp";
 import { ToursGallery } from "../pages/tours/HomeToursGallery/ToursGallery";
+import { UserProfile } from "../pages/users/UserProfile/UserProfile";
 
 export const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ export const AppRoutes = () => {
         <NavBarApp />
         <Routes>
           <Route path="/" element={<ToursGallery />} />
+          <Route path="/users/userprofile" element={<UserProfile />} />
         </Routes>
       </Row>
     </BrowserRouter>
--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -4,9 +4,21 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 class usersControllers {
-
-
+  viewProfile = (req, res) => {
+    console.log("este es tu perfil personal");
+  };
+  ownTours = (req, res) => {
+    console.log("estas son mis guías");
+  };
+  favTours = (req, res) => {
+    console.log("estas son mis guías favoritas");
+  };
+  boughtTours = (req, res) => {
+    console.log("estas son mis guías adquiridas");
+  };
+  editUser = (req, res) => {
+    console.log("aqui puedes editar tu usuario");
+  };
 }
-
 
 module.exports = new usersControllers();
