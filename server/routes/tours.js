92f1a26e9a6d69ddbe5ffea4ae2f95815dbc35af
celia <celiagarcia0315@gmail.com>
2024-01-19 09:53:43 +0100

    inicioaddtour

--- server/routes/tours.js
+++ server/routes/tours.js
@@ -2,6 +2,6 @@ var express = require("express");
 const toursControllers = require("../controllers/toursControllers");
 var router = express.Router();
 
-router.get("/newtour", toursControllers.newTour);
+router.post("/newtour", toursControllers.newTour);
 
 module.exports = router;
