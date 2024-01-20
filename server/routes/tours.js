4989726c6a2173b31533606a154c837b91c21b54
edson <edsonaristiguieta@gmail.com>
2024-01-20 14:42:04 +0100

    Creacion de guia listo

--- server/routes/tours.js
+++ server/routes/tours.js
@@ -1,8 +1,17 @@
 var express = require("express");
 const toursControllers = require("../controllers/toursControllers");
+const multerSingle = require("../middleware/multerSingle");
+const multerMulti = require("../middleware/multerMulti");
+
 var router = express.Router();
 
 //ruta base http://localhost:3000/tours/
-router.post("/newtour", toursControllers.newTour);
+router.post("/newtour", multerSingle("tours"), toursControllers.newTour);
+
+router.put(
+  "/addsection/:tour_id",
+  multerMulti("section"),
+  toursControllers.addSection
+);
 
 module.exports = router;
