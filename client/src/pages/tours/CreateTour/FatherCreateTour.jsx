fa025d0bb04940fce78c502c30259bd930fd36e6
edson <edsonaristiguieta@gmail.com>
2024-01-22 14:49:58 +0100

    Section

--- client/src/pages/tours/CreateTour/FatherCreateTour.jsx
+++ client/src/pages/tours/CreateTour/FatherCreateTour.jsx
@@ -3,14 +3,41 @@ import { CreateTour } from "./CreateTour";
 import { CreateSection } from "./CreateSection";
 
 export const FatherCreateTour = () => {
+  const [showSections, setShowSections] = useState(false);
   const [showFormSection, setShowFormSection] = useState(false);
+  const [sections, setSections] = useState([]);
 
+  const [tour, setTour] = useState({});
+
+  console.log("padreee", tour);
+  console.log(sections);
   return (
     <>
-      <CreateTour />
+      {!showSections && (
+        <CreateTour
+          setShowSections={setShowSections}
+          tour={tour}
+          setTour={setTour}
+        />
+      )}
 
-      <button onClick={() => setShowFormSection(true)}> Añadir punto </button>
-      {showFormSection && <CreateSection />}
+      {showSections && (
+        <>
+          <h2> {tour?.tour_name} </h2>
+          {sections.map((elem) => {
+            return <p>{elem.section_name}</p>;
+          })}
+          <button onClick={() => setShowFormSection(true)}>Añadir punto</button>
+          {showFormSection && (
+            <CreateSection
+              setShowFormSection={setShowFormSection}
+              tour={tour}
+              sections={sections}
+              setSections={setSections}
+            />
+          )}
+        </>
+      )}
     </>
   );
 };
