f49b4ceee4312a426a357441c6c05d3aaa64fbc9
edson <edsonaristiguieta@gmail.com>
2024-01-22 08:50:37 +0100

    m

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,8 @@ import { AppRoutes } from "./Routes/AppRoutes";
 import { Container } from "react-bootstrap";
 import { KankooProvider } from "./context/KankooContext";
 
-<<<<<<< HEAD
-=======
 import "./App.scss";
 
->>>>>>> 332799c62ef3f5c9c1dbaa7ee14b557da9311611
 function App() {
   return (
     <Container fluid>
