f3dc0e232d5f6bd4d149b5ff48e360ff101e6aa9
Estrella <estrellaparra89@hotmail.com>
2024-01-24 16:16:03 +0100

    estadísticas de admin

--- client/src/pages/admin/AdminStats/AdminStats.jsx
+++ client/src/pages/admin/AdminStats/AdminStats.jsx
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { KankooContext } from "../../../context/KankooContext";
+import { Button, Col, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export const AdminStats = () => {
+  const [totalUsers, setTotalUsers] = useState();
+  const [totalUsersEnabled, setTotalUsersEnabled] = useState();
+  const [totalUsersDisabled, setTotalUsersDisabled] = useState();
+  const { adminUsers, allTours } = useContext(KankooContext);
+  const [totalTours, setTotalTours] = useState();
+  const [totalToursEnabled, setTotalToursEnabled] = useState();
+  const [totalToursDisabled, setTotalToursDisabled] = useState();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTotalUsers(adminUsers?.length);
+    setTotalUsersEnabled(adminUsers?.filter((e) => e.is_deleted === 0).length);
+    setTotalUsersDisabled(adminUsers?.filter((e) => e.is_deleted === 1).length);
+    setTotalTours(allTours?.length);
+    setTotalToursEnabled(allTours?.filter((e) => e.is_deleted === 0).length);
+    setTotalToursDisabled(allTours?.filter((e) => e.is_deleted === 1).length);
+  }, [adminUsers, allTours]);
+
   return (
-    <div>AdminStats</div>
-  )
-}
+    <>
+      <Row>
+        <Col>
+          <h2>Estadísticas:</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div>
+            <h3>Usuarios Totales</h3>
+            {totalUsers}
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <h3>Usuarios Activados</h3>
+            {totalUsersEnabled}
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <h3>Usuarios Desactivados</h3>
+            {totalUsersDisabled}
+            <Button onClick={() => navigate("/admin/adminUsers")}>
+              Ver Listado de Usuarios
+            </Button>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div>
+            <h3>Guías Totales</h3>
+            {totalTours}
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <h3>Guías activas</h3>
+            {totalToursEnabled}
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <h3>Guías a la espera de aprobación</h3>
+            {totalToursDisabled}
+            <Button onClick={() => navigate("/admin/adminTours")}>
+              Ver Listado de Guías
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -3,11 +3,63 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
-
 class adminControllers {
+  getAllUsers = (req, res) => {
+    let sql = "SELECT * FROM user where type = 2";
+    connection.query(sql, (error, result) => {
+      console.log(result);
+      if (error) {
+        res.status(400).json({ error });
+      }
+      res.status(200).json(result);
+    });
+  };
+  //deshabilita un usuario
+  disableUser = (req, res) => {
+    console.log(req.params);
 
+    let { id } = req.params;
+    console.log(id);
+    let sql = `UPDATE user SET is_deleted = 1 WHERE user_id = "${id}"`;
+    let sql2 = "SELECT * from user where type=2";
 
+    connection.query(sql, (error, result) => {
+      if (error) throw error;
+      console.log(error);
+    });
+    connection.query(sql2, (error, resultUsers) => {
+      error
+        ? res.status(400).json({ error })
+        : res.status(200).json(resultUsers);
+    });
+  };
+
+  enableUser = (req, res) => {
+    console.log(req.params);
+
+    let { id } = req.params;
+    console.log(id);
+    let sql = `UPDATE user SET is_deleted = 0 WHERE user_id = "${id}"`;
+    let sql2 = "SELECT * from user where type = 2";
+
+    connection.query(sql, (error, result) => {
+      if (error) throw error;
+      console.log(error);
+    });
+    connection.query(sql2, (error, resultUsers) => {
+      error
+        ? res.status(400).json({ error })
+        : res.status(200).json(resultUsers);
+    });
+  };
+
+  //trae todas las fotos de los usuarios
+  getAllPics = (req, res) => {
+    let sql = "SELECT * FROM avatar";
+    connection.query(sql, (error, result) => {
+      error ? res.status(400).json({ error }) : res.status(200).json(result);
+    });
+  };
 }
-
 
 module.exports = new adminControllers();
