31ad2c0fb292b709adf8d9844ac4ac78462b62f5
Estrella <estrellaparra89@hotmail.com>
2024-01-26 09:28:31 +0100

    Admin Users y Admin Stats

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -130,6 +130,7 @@ class usersControllers {
         res.status(401).json("Email no registrado");
       } else {
         const user = result[0];
+        console.log(user);
         const hash = user.password;
         bcrypt.compare(password, hash, (error, response) => {
           if (error) return res.status(500).json(error);
@@ -138,7 +139,8 @@ class usersControllers {
               {
                 user: {
                   id: user.user_id,
-                  type: user.type,
+                  type: user.user_type,
+                  prueba: "hola",
                 },
               },
               process.env.SECRET,
