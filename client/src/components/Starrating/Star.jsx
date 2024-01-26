afb1f2e97592ab034e2311a4be9bd5bf5e16fc95
celia <celiagarcia0315@gmail.com>
2024-01-26 17:29:25 +0100

    rating

--- client/src/components/Starrating/Star.jsx
+++ client/src/components/Starrating/Star.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
-export const Star = ({ selected = false, onSelect = (f) => f }) => {
+export const Star = ({
+  setSelectedStars,
+  selected = false,
+  onSelect = (f) => f,
+  tour_id,
+  id,
+}) => {
+  const handleClickRating = () => {
+    onSelect();
+    setSelectedStars((prevSelectedStars) => {
+      console.log("COSILLAS", prevSelectedStars, tour_id, id);
+
+      axios
+        .post(`http://localhost:3000/tours/${tour_id}/rating/${id}`, {
+          prevSelectedStars,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      return prevSelectedStars;
+    });
+  };
+
   return (
-    <p
-      style={{ fontSize: "50px", color: selected ? "#f07674" : "gray" }}
-      onClick={onSelect}
+    <span
+      className={selected ? "starSelected" : "starNotSelected"}
+      aria-label="icono de estrella de valoración"
+      onClick={handleClickRating}
     >
       &#9733;
-    </p>
+    </span>
   );
 };
