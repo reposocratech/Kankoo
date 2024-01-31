396ef5356351f4984edf2146aaa5283ef386270f
chelu <cheluhg@gmail.com>
2024-01-31 15:06:28 +0100

    arreglos varios

--- client/src/pages/tours/OneSection/AudioSection.jsx
+++ client/src/pages/tours/OneSection/AudioSection.jsx
@@ -3,16 +3,11 @@ import React from "react";
 export const AudioSection = ({ sectionResources }) => {
   return (
     <>
-      <audio controls className="custom-audio-player">
-        <source
-          src={`http://localhost:3000/resources/audios/${sectionResources.text}`}
-          type="audio/ogg"
-        />
-        <source
-          src={`http://localhost:3000/resources/audios/${sectionResources.text}`}
-          type="audio/mpeg"
-        />
-      </audio>
+      <audio
+        controls
+        src={`http://localhost:3000/resources/audios/${sectionResources.text}`}
+        className="custom-audio-player"
+      ></audio>
     </>
   );
 };
