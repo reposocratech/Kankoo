ed72fb733628340ac1bf1283f10979837811c3e6
edson <edsonaristiguieta@gmail.com>
2024-02-01 13:31:04 +0100

    Cambios OneSec

--- client/src/pages/tours/OneSection/AudioSection.jsx
+++ client/src/pages/tours/OneSection/AudioSection.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const AudioSection = ({ sectionResources }) => {
   return (
-    <>
+    <div>
       <audio
         controls
         src={`http://localhost:3000/resources/audios/${sectionResources.text}`}
         className="custom-audio-player"
       ></audio>
-    </>
+    </div>
   );
 };
