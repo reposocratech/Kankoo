cb9b7bea32c06933c89d85226ec04766af84cd04
celia <celiagarcia0315@gmail.com>
2024-02-01 14:00:14 +0100

    aa

--- client/src/components/Navbar/NavbarApp.jsx
+++ client/src/components/Navbar/NavbarApp.jsx
@@ -9,7 +9,6 @@ import { delLocalStorage } from "../../../helpers/localStorageUtils";
 
 function NavBarApp() {
   const { user, setUser, token, setIsLogged } = useContext(KankooContext);
-
   const navigate = useNavigate();
   const handleLogout = () => {
     delLocalStorage("token");
@@ -17,7 +16,6 @@ function NavBarApp() {
     setIsLogged(false);
     navigate("/");
   };
-
   const handleAvatarClick = () => {
     if (user?.user_type === 1) {
       navigate("/admin/adminProfile");
@@ -71,7 +69,6 @@ function NavBarApp() {
       );
     }
   };
-  /* jolines */
   const renderAdminContent = () => {
     if (token && user?.user_type === 1) {
       return (
